0c8a3754eec57b92c894a8fc0656ddc28a452923
dzyjiayou <1564138025@qq.com>
2025-05-12 21:49:36 +0800

    ChatPDF和给聊天加入图片完成

--- src/main/java/com/itheima/ai/config/CommonConfiguration.java
+++ src/main/java/com/itheima/ai/config/CommonConfiguration.java
@@ -4,11 +4,17 @@ import com.itheima.ai.constants.SystemConstants;
 import com.itheima.ai.tools.CourseTools;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,14 +25,20 @@ public class CommonConfiguration {
         return new InMemoryChatMemory();
     }
     @Bean
-    public ChatClient chatClient(OllamaChatModel model, ChatMemory chatMemory) {
+    public ChatClient chatClient(OpenAiChatModel model, ChatMemory chatMemory) {
         return ChatClient.builder(model) // 创建ChatClient工厂实例
-                .defaultSystem("您是一家名为“黑马程序员”的职业教育公司的客户聊天助手，你的名字叫小黑。请以友好、乐于助人和愉快的方式解答学生的各种问题。")
-                .defaultAdvisors(new SimpleLoggerAdvisor(),
-                        new MessageChatMemoryAdvisor(chatMemory)) // 添加默认的Advisor,记录日志
+                .defaultOptions(ChatOptions.builder().model("qwen-omni-turbo").build())
+                .defaultSystem("您是一家名为“黑马程序员”的职业教育公司的客户聊天助手，你的名字叫小黑。请以友好、乐于助人和愉快的方式解答用户的各种问题。")
+                .defaultAdvisors(new SimpleLoggerAdvisor()) // 添加默认的Advisor,记录日志
                 .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
                 .build(); // 构建ChatClient实例
+
     }
+    @Bean
+    public VectorStore vectorStore(OpenAiEmbeddingModel embeddingModel) {
+        return SimpleVectorStore.builder(embeddingModel).build();
+    }
+
     @Bean
     public ChatClient serviceChatClient(OpenAiChatModel model, ChatMemory chatMemory, CourseTools courseTools) {
         return ChatClient.builder(model) // 创建ChatClient工厂实例
@@ -35,5 +47,25 @@ public class CommonConfiguration {
                         new MessageChatMemoryAdvisor(chatMemory)) // 添加默认的Advisor,记录日志
                 .defaultTools(courseTools)
                 .build(); // 构建ChatClient实例
+    }
+    @Bean
+    public ChatClient pdfChatClient(
+            OpenAiChatModel model,
+            ChatMemory chatMemory,
+            VectorStore vectorStore) {
+        return ChatClient.builder(model)
+                .defaultSystem("请根据提供的上下文回答问题，不要自己猜测。")
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory), // CHAT MEMORY
+                        new SimpleLoggerAdvisor(),
+                        new QuestionAnswerAdvisor(
+                                vectorStore, // 向量库
+                                SearchRequest.builder() // 向量检索的请求参数
+                                        .similarityThreshold(0.5d) // 相似度阈值
+                                        .topK(2) // 返回的文档片段数量
+                                        .build()
+                        )
+                )
+                .build();
     }
 }
--- src/main/java/com/itheima/ai/controller/ChatController.java
+++ src/main/java/com/itheima/ai/controller/ChatController.java
@@ -7,11 +7,20 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.model.Media;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,11 +30,39 @@ public class ChatController {
     private final ChatHistoryRepository chatHistoryRepository;
 
     @RequestMapping(value = "/chat", produces = "text/html;charset=utf-8")
-    public Flux<String> chat(String prompt, String chatId) {
+    public Flux<String> chat(
+            @RequestParam("prompt")String prompt,
+            @RequestParam("chatId")String chatId,
+            @RequestParam(value = "files", required = false) List<MultipartFile> files) {
         chatHistoryRepository.save("chat", chatId);
-        return chatClient.prompt().user(prompt)
-                .advisors(a->a.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY, chatId))
-                .stream().content();
+        if(files == null || files.isEmpty()) {
+            return textChat(prompt, chatId);
+        } else {
+            return multiModelChat(prompt, chatId, files);
+        }
+    }
 
+    private Flux<String> multiModelChat(String prompt, String chatId, List<MultipartFile> files) {
+        List<Media> medias = files.stream()
+                .map(file -> new Media(
+                                MimeType.valueOf(Objects.requireNonNull(file.getContentType())),
+                                file.getResource()
+                        )
+                )
+                .toList();
+        // 2.请求模型
+        return chatClient.prompt()
+                .user(p -> p.text(prompt).media(medias.toArray(Media[]::new)))
+                .advisors(a -> a.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId))
+                .stream()
+                .content();
+    }
+
+    private Flux<String> textChat(String prompt, String chatId) {
+        return chatClient.prompt()
+                .user(prompt)
+                .advisors(a -> a.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId))
+                .stream()
+                .content();
     }
 }
